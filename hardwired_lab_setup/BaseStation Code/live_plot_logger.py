6fd2de3a833a5bf02fe35c8642a706fe11fc389e
jacksonpricewv <jap00033@mix.wvu.edu>
2025-04-29 13:26:44 -0400

    Fixes serial port with linux support

--- hardwired_lab_setup/BaseStation Code/live_plot_logger.py
+++ hardwired_lab_setup/BaseStation Code/live_plot_logger.py
@@ -30,7 +30,7 @@ def convert_voltage_to_temperature(voltage):
 def find_serial_port():
     ports = list(serial.tools.list_ports.comports())
     for port in ports:
-        if "USB" in port.device or "ttyUSB" in port.device or "ttyACM" in port.device:
+        if "USB" in port.description or "ttyUSB" in port.description or "ttyACM" in port.description:
             return port.device
     raise Exception("No suitable serial port found")
 
